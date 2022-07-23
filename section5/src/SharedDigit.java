c5451e22259a49d7fe7b4e3a2786a7c47c9d7384
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-07-23 15:45:08 -0300

    cria as variaveis booleanas para validar a igualdade entre os digitos dos numeros, insere comentarios explicando o raciocinio

--- section5/src/SharedDigit.java
+++ section5/src/SharedDigit.java
@@ -11,26 +11,44 @@ EXAMPLE INPUT/OUTPUT:
 * hasSharedDigit(15, 55); → should return true since the digit 5 appears in both numbers
 */
     public static void main(String[] args) {
-        System.out.println(hasSharedDigit(23, 23));
+        System.out.println(hasSharedDigit(60, 16));
     }
     public static boolean hasSharedDigit(int num1, int num2) {
 
         boolean isValidNumbers = num1 > 9 && num1 < 100 && num2 > 9 && num2 < 100;
         if (!isValidNumbers) {
+            System.out.println("out of range");
             return false;
         }
-        int compareNumbers = 0;
-        while (num1 > 0) {
+
+        // condicao para o loop seja interrompido antes do resultado da divisao por 10 for igual a zero
+        while (num1 / 10 >= 1) {
+            // armazena os digitos das nunidades
+            int digitUnidadeNum1 = num1 % 10;
+            int digitUnidadeNum2 = num2 % 10;
+            // armazena o digito das dezenas
+            num1 /= 10;
+            num2 /= 10;
+            // validacao de igualdade entre o algarimos da unidade do 1° numero com ambos algarismos do 2°
+            boolean isUnityEqualUnityOrTens = digitUnidadeNum1 == digitUnidadeNum2 || digitUnidadeNum1 == num2;
+            // validacao de igualdade entre o algarimos da dezena do 1° numero com ambos algarismos do 2°
+            boolean isTensEqualUnityOrTens = num1 == num2 || num1 == digitUnidadeNum2;
+            if (isTensEqualUnityOrTens || isUnityEqualUnityOrTens) {
+                System.out.println("Ha algarismo semelhante entre os numeros");
+                return true;
+            }
+        }
+        System.out.println("Não ha nenhum algarismo semelhante entre os numeros");
+        return false;
+    }
+}
+/*  Essa logica compara digitos de mesma posicao no numero EX. dezena/dezena, unidade/unidade
+*         boolean compareNumbers = true;
+*         while (num1 > 1) {
             int digitsNum1 = num1 % 10;
             int digitsNum2 = num2 % 10;
             num1 /= 10;
-            System.out.println("num1 " + digitsNum1);
             num2 /= 10;
-            System.out.println("num2 " + digitsNum2);
-            if (digitsNum1 == digitsNum2) {
-                System.out.printf("num1 %d num2 %d", num1, num2);
-            }
-        }
-        return true;
-    }
-}
+*           compareNumbers = digitsNum1 == digitsNum2;
+*       }
+* */
