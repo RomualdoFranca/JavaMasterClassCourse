bbdede3b72e0c20742ffc5f47db64bef2533ddf9
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-07-23 10:32:31 -0300

    refatora a condicao do while loop para retornar true os palindromos negativos

--- section5/src/NumberPalindrome.java
+++ section5/src/NumberPalindrome.java
@@ -29,51 +29,67 @@ Tip: Be careful with reversing a number, you will need a parameter for comparing
 (parameter).
 */
     public static void main(String[] args) {
-        System.out.println(isPalindrome(-77));
+        System.out.println(isPalindrome(-73437));
+        System.out.println(isPalindrome(73437));
+        System.out.println(isPalindrome(101));
+        System.out.println(isPalindrome(123));
+        System.out.println(isPalindrome(9));
+        System.out.println(isPalindrome(-9));
+
     }
     public static boolean isPalindrome(int number) {
 
+
+    //  Solucao sem os comentarios
         if (number > -10 && number < 10) {
+            System.out.println("Number with one digit can't be a palindrome");
             return false;
         }
         int reverse = 0;
         int numberNoIncrement = number;
-
-        System.out.println(numberNoIncrement);
-        while (number > 0) {
-            // Para se inverter um numero: multiplicar cada algarismo por 10 e somar com o proximo algarismo encontrado
-            // Ex: 1° algarismo encontrado vezes 10 + 2° algarismo vezes 10, e assim sucessivamente
-            // essa variavel armazena o numero inverso
+        while (number != 0) {
             reverse *= 10;
-            // formula para pegar o ultimo algarismo do numero
             int digit = number % 10;
-            // formula para retirar o ultimo algarismo do numero
             number /= 10;
-            // formula que adiciona os algarismo ja multiplicados por 10
             reverse += digit;
-            System.out.println("reverse " + reverse);
         }
-        System.out.println("number " + numberNoIncrement);
         if (reverse == numberNoIncrement) {
-            System.out.println("The number is a palindrome ");
+            System.out.printf("The number %d is a palindrome \n", numberNoIncrement);
             return true;
         }
-        System.out.println("The number isn't a palindrome ");
+        System.out.printf("The number %d isn't a palindrome \n", numberNoIncrement);
         return false;
     }
-}
-/*        int lastDigit = number % 10;;
-//        System.out.println("last digit " + lastDigit);
-        int digit = 0;
-        while (number > 9) {
 
-            System.out.println("numero antes do incremento " + number);
-            number /= 10;
-            System.out.println("numero após incremento  = " + number);
-            digit = number % 10;
-            System.out.println("digit = " + digit);
-            System.out.println("");
-        }
-        System.out.println("last digit = " + lastDigit);
-        System.out.println("number fora do loop " + number);
-        return true;*/
+//        if (number > -10 && number < 10) {
+//            System.out.println("Number with one digit can't be a palindrome");
+//            return false;
+//        }
+//        int reverse = 0;
+//        int numberNoIncrement = number;
+////        System.out.println(numberNoIncrement);
+//        // Palindromo negativos não estavam retornando true pq a condicao do while 'while(number > 0)' só estava considerando
+//        // numeros positivos.
+//        while (number != 0) {
+//            // Para se inverter um numero: multiplicar cada algarismo por 10 e somar com o proximo algarismo encontrado
+//            // Ex: 1° algarismo encontrado vezes 10 + 2° algarismo vezes 10, e assim sucessivamente
+//            // essa variavel armazena o numero inverso
+//            reverse *= 10;
+//            // formula para pegar o ultimo algarismo do numero
+//            int digit = number % 10;
+//            // formula para retirar o ultimo algarismo do numero
+//            number /= 10;
+//            // formula que adiciona os algarismo ja multiplicados por 10
+//            reverse += digit;
+////            System.out.println("reverse " + reverse);
+//        }
+////        System.out.println("number " + numberNoIncrement);
+//        if (reverse == numberNoIncrement) {
+//            System.out.printf("The number %d is a palindrome \n", numberNoIncrement);
+//            return true;
+//        }
+//        System.out.printf("The number %d isn't a palindrome \n", numberNoIncrement);
+//        return false;
+//    }
+}
+
