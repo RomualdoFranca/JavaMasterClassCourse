3af9e529c0c9107899017f9b4e63b0281593ef96
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-07-26 20:47:58 -0300

    implementa a logica da divisao com resultados exatos sem resto

--- myIdeas/src/ListaDeDivisores.java
+++ myIdeas/src/ListaDeDivisores.java
@@ -1,19 +1,30 @@
 public class ListaDeDivisores {
 
     public static void main(String[] args) {
-        System.out.println(listaDivisores(12, 20));
+        System.out.println(listaDivisores(32));
     }
-    public static int listaDivisores(int num, int num2) {
+    public static int listaDivisores(int num) {
         int count = 0;
         int i = 2;
         int numInteiro = num;
         // fazer a busca do divisores do numero ate no maximo a metade do seu valor
         // retornar o ultimo valor que tenha a divisao exata
-        for ( i = 2; i <= num / 2; i++) {
-            // a quantidade de loops deve ser definida a partir do menor numero
+        for ( i = 2; i <= numInteiro / 2; i++) {
             count++;
-            System.out.printf("%d° loop %d/%d\n",count, num, i);
-            numInteiro /= i;
+            System.out.println("quosciente " + num);
+//            System.out.printf("%d° loop: Divisores : %d/%d\n",count, numInteiro, i);
+            num = numInteiro / i;
+            // implementa a logica para considerar apenas os divisores com quoscientes exatos
+            if (numInteiro % i == 0) {
+//                System.out.println("numInteiro " + numInteiro);
+                System.out.printf("%d° loop:Divisao exata; Divisores : %d\n",count, i);
+//                System.out.printf("%d° loop: divisao exata \n",count);
+
+//                count++;
+//                numInteiro /= i;
+//                System.out.printf("%d° loop: Divisores sem resto: %d/%d\n",count, num, i);
+
+            }
         }
 
 
