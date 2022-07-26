0bbf05f55a8a91f499513338da4919d256ae07de
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-07-26 19:55:20 -0300

    implementa o loop que lista todos os divisores do numero, com excessao do 1 e dele mesmo

--- myIdeas/src/ListaDeDivisores.java
+++ myIdeas/src/ListaDeDivisores.java
@@ -4,15 +4,16 @@ public class ListaDeDivisores {
         System.out.println(listaDivisores(12, 20));
     }
     public static int listaDivisores(int num, int num2) {
-
-        int count = num;
+        int count = 0;
         int i = 2;
-        int remainderNum = 0;
-        for ( i = 2; i <= num; i++) {
+        int numInteiro = num;
+        // fazer a busca do divisores do numero ate no maximo a metade do seu valor
+        // retornar o ultimo valor que tenha a divisao exata
+        for ( i = 2; i <= num / 2; i++) {
             // a quantidade de loops deve ser definida a partir do menor numero
             count++;
             System.out.printf("%d° loop %d/%d\n",count, num, i);
-            num /= i;
+            numInteiro /= i;
         }
 
 
