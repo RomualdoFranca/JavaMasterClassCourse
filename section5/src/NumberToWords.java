e3b6c4c0fa2da2cd4f5caf34cabf5df44e10be97
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-08-01 19:41:18 -0300

    convert the value into a word in reverse order

--- section5/src/NumberToWords.java
+++ section5/src/NumberToWords.java
@@ -61,17 +61,55 @@ They don't have to be separated by a space.
     public static void main(String[] args) {
 //        numberToWords(-3);
 //        numberToWords(1);
-        numberToWords(39);
+        numberToWords(1234567890);
     }
     public static void numberToWords(int number) {
         if (number < 0 ) {
-            System.out.println("Invalid Value");
+            System.out.print("Invalid Value");
         }
-        //Extract the last digit
-        int lastDigit = number % 10;
-        System.out.println("the last digit " + lastDigit);
-        //Remove the last digit
-        number /= 10;
-        System.out.println("remove the last digit " + number);
+//        System.out.print("the last digit " + lastDigit);
+        while (number > 0) {
+            //Extract the last digit
+            int lastDigit = number % 10;
+            //Convert the value of the digit into a word
+            switch (lastDigit) {
+                case 0:
+                    System.out.print("Zero ");
+                    break;
+                case 1:
+                    System.out.print("One ");
+                    break;
+                case 2:
+                    System.out.print("Two ");
+                    break;
+                case 3:
+                    System.out.print("Three ");
+                    break;
+                case 4:
+                    System.out.print("Four ");
+                    break;
+                case 5:
+                    System.out.print("Five ");
+                    break;
+                case 6:
+                    System.out.print("Six ");
+                    break;
+                case 7:
+                    System.out.print("Seven ");
+                    break;
+                case 8:
+                    System.out.print("Eight ");
+                    break;
+                case 9:
+                    System.out.print("Nine ");
+                    break;
+
+            }
+            //Remove the last digit
+            number /= 10;
+//            System.out.print("removed the last digit =" + number);
+
+        }
+
     }
 }
